73d8eaf4f18b5fb490cf6d7c1356bbe0f3179417
iajaygehlot24 <starajgehlot@gmail.com>
2025-05-21 20:02:05 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,11 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "10634878"))
+API_HASH = environ.get("API_HASH", "2eab99b8459017fff27395cc52f3c860")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7741816221:AAHxPLXrt0HjfG7MTrntqcXYsDDRNN5-BT8")
+OWNER = int(environ.get("OWNER", "1168219996"))
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
